576d10e47f15710270c0d9b6a82121e1409ce62e
Henrique Cecconi <henrique.cecconi@gmail.com>
2023-03-07 19:32:46 -0300

    Adding dummy data to learn how to use Jinja inside our templates

--- flask_blog.py
+++ flask_blog.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Henrique Cecconi',
+        'title': 'First Blog Post',
+        'content': 'Fisrt post content',
+        'date_posted': '07/03/2023'
+    },
+    {
+        'author': 'Caio Castro',
+        'title': 'Second Blog Post',
+        'content': 'Second post content',
+        'date_posted': '07/03/2023'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
     
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
