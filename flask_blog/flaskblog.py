700d64708dd41b19a310f2d53f9e991f048ca5e9
Henrique Cecconi <58144622+HenriqueCecconi@users.noreply.github.com>
2024-01-11 23:22:44 -0300

    Create a basic template with dummy data
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Henrique Cecconi',
+        'title': 'Blog Post 1',
+        'content': 'My first blog post!',
+        'date_posted': '2023-01-10'
+    },
+    {
+        'author': 'Henrique Cecconi',
+        'title': 'Blog Post 2',
+        'content': 'My second blog post!',
+        'date_posted': '2023-01-11'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
