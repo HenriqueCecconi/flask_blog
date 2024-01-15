bd45a5738194aa25781a9dfaa447277bc89abdee
Henrique Cecconi <58144622+HenriqueCecconi@users.noreply.github.com>
2024-01-14 23:17:03 -0300

    Create forms to login and register to the blog
    There is some dummy data used for testing the routes, forms and alerts.
    Not production ready yet. Later I'll setup a database and remove them.

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,26 +1,60 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, flash, redirect, render_template, url_for
+from forms import LoginForm, RegistrationForm
+
 app = Flask(__name__)
 
 posts = [
     {
-        'author': 'Henrique Cecconi',
-        'title': 'Blog Post 1',
-        'content': 'My first blog post!',
-        'date_posted': '2023-01-10'
+        "author": "Henrique Cecconi",
+        "title": "Blog Post 1",
+        "content": "My first blog post!",
+        "date_posted": "2023-01-10",
     },
     {
-        'author': 'Henrique Cecconi',
-        'title': 'Blog Post 2',
-        'content': 'My second blog post!',
-        'date_posted': '2023-01-11'
-    }
+        "author": "Henrique Cecconi",
+        "title": "Blog Post 2",
+        "content": "My second blog post!",
+        "date_posted": "2023-01-11",
+    },
 ]
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html', posts=posts)
 
-@app.route('/about')
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template("about.html", title="About")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", category="success")
+            return redirect(url_for("home"))
+        else:
+            flash(
+                "Login failed. Please check your email and password",
+                category="danger",
+            )
+
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", category="success")
+        return redirect(url_for("home"))
+
+    return render_template("register.html", title="Register", form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
