c5d489c14626cdd8ead240c781ee20640c91adcf
Henrique Cecconi <58144622+HenriqueCecconi@users.noreply.github.com>
2024-01-25 20:37:02 -0300

    Make secret key an environment variable

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,9 +1,13 @@
+from os import environ
+
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "super_secret"
+app.config["SECRET_KEY"] = environ.get("FLASK_BLOG_SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flask_blog import routes
